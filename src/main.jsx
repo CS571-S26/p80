f4bf290d0dceff4bb1ebf0c850696f4520a11297
Dylan Coy <coyrdylan@gmail.com>
2026-03-16 13:43:43 -0500

    deployment setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client'
 import App from './Components/App.jsx'
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-  <App></App>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
